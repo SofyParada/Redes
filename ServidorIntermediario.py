79f1b625ada18bb0b4ab08262aea29f7d2660155
SofyParada <96263962+SofyParada@users.noreply.github.com>
2023-09-09 19:12:18 -0300

    ServidorIntermediario.py

--- ServidorIntermediario.py
+++ ServidorIntermediario.py
@@ -7,6 +7,8 @@ mi_socket.listen(5)
 
 
 
+
+
 while True:
     conexion, addr = mi_socket.accept()
     print("Nueva conexion establecida")
@@ -20,8 +22,15 @@ while True:
     mensaje_bytes = mensaje.encode('utf-8')
     conexion.send(mensaje_bytes)
     
-    mensaje2 = "Hola, servidor go!"
-    mi_socket.sendto(mensaje2.encode('utf-8'), (socket.gethostname(), 8000))
     
+    mensaje2 = "Hola, servidor go!"
+    mi_socket2.sendto(mensaje2.encode('utf-8'), ('127.0.0.1', 1234))
+    
+    data, address = mi_socket2.recvfrom(1024)
+    print(f"Mensaje recibido de {address}: {data.decode('utf-8')}")
     
     conexion.close()
+    mi_socket2.close()
+    
+
+    
