4b917deaf7276bae199e8776d623b78cf1e1a7ff
SofyParada <96263962+SofyParada@users.noreply.github.com>
2023-09-09 21:42:45 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,16 +1,28 @@
 import socket
+import numpy as np
 
 mi_socket = socket.socket()
 mi_socket.connect( (socket.gethostname(), 8000) )
 
 print("- - - - - - - - Bienvenido al Juego - - - - - - - -")
-Seleccion =input("- Seleccione una opción\n1-Jugar\n2-Salir")
+Seleccion =input("- Seleccione una opción\n1-Jugar\n2-Salir\n")
 if Seleccion == '1':
     Seleccion_bytes = Seleccion.encode('utf-8')
     mi_socket.send(Seleccion_bytes)
     
     respuesta = mi_socket.recv(1024)
-    print("Respuesta de disponibilidad: ", respuesta)    
+    print("Respuesta de disponibilidad: ", respuesta,"\n")
+    print("- - - - - - - - Comienza el Juego - - - - - - - -")
+    
+    tablero = np.array([[0,0,0,0,0,0], 
+               [0,0,0,0,0,0], 
+               [0,0,0,0,0,0],
+               [0,0,0,0,0,0],
+               [0,0,0,0,0,0],
+               [0,0,0,0,0,0]])
+    print("Tablero inicial:\n",tablero)
+    
+    
 else:
     
     Seleccion_bytes = Seleccion.encode('utf-8')
