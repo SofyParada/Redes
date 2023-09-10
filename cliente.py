fa472db4c25ae6a339ea29282ddb280cea753b2a
SofyParada <96263962+SofyParada@users.noreply.github.com>
2023-09-09 21:00:57 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -3,6 +3,23 @@ import socket
 mi_socket = socket.socket()
 mi_socket.connect( (socket.gethostname(), 8000) )
 
+print("- - - - - - - - Bienvenido al Juego - - - - - - - -")
+Seleccion =input("- Seleccione una opción\n1-Jugar\n2-Salir")
+if Seleccion == '1':
+    Seleccion_bytes = Seleccion.encode('utf-8')
+    mi_socket.send(Seleccion_bytes)
+    
+    respuesta = mi_socket.recv(1024)
+    print("Respuesta de disponibilidad: ", respuesta)    
+else:
+    
+    Seleccion_bytes = Seleccion.encode('utf-8')
+    mi_socket.send(Seleccion_bytes)
+    
+    respuesta = str(mi_socket.recv(1024))
+    print("Respuesta de disponibilidad: ", respuesta)    
+
+'''
 mensaje = "Hola, te saludo desde el cliente!"
 mensaje_bytes = mensaje.encode('utf-8')
 
@@ -11,4 +28,5 @@ respuesta = mi_socket.recv(1024)
 
 print(respuesta)
 mi_socket.close()
-
+    
+'''
